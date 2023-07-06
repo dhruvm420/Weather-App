d956d17891f2658f4d34c39dbad092184787a573
Dhruv <mahajandhruv420@gmail.com>
2023-07-06 10:07:53 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const wind_speed = document.getElementById('wind-speed');
+
+const location_not_found = document.querySelector('.location-not-found');
+
+const weatherOutput = document.querySelector('.weather-output');
+
+
+async function checkWeather(city){
+    const api_key = "0095258f1bdd39fdcdf98713501558b4";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+
+
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weatherOutput.style.display = "none";
+        console.log("error");
+        return;
+    }
+
+    console.log(weather_data);
+    console.log("run");
+    location_not_found.style.display = "none";
+    weatherOutput.style.display = "flex";
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
+
+    var condition = weather_data.weather[0].main;
+    switch(condition){
+        case 'Clouds':
+            weather_img.src = "/assets/cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "/assets/clear.png";
+            break;
+        case 'Rain':
+            weather_img.src = "/assets/rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "/assets/mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "/assets/snow.png";
+            break;
+
+    }
+
+    console.log(weather_data);
+}
+
+inputBox.addEventListener("keydown", function (e) {
+    if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+        checkWeather(inputBox.value);
+    }
+});
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});
