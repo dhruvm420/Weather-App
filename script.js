ab9997d52b9bd78d431d382cf70494a2aacfdab4
Dhruv <mahajandhruv420@gmail.com>
2023-07-07 02:10:58 +0530

    [Fix] weather image not changing accordingly

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const searchBtn = document.getElementById('searchBtn');
 const weather_img = document.querySelector('.weather-img');
 const temperature = document.querySelector('.temperature');
 const description = document.querySelector('.description');
-const humidity = document.getElementById('humidity');
+const humidity = document.getElementById('humidity-value');
 const wind_speed = document.getElementById('wind-speed');
 
 const location_not_found = document.querySelector('.location-not-found');
@@ -31,26 +31,25 @@ async function checkWeather(city){
     weatherOutput.style.display = "flex";
     temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
     description.innerHTML = `${weather_data.weather[0].description}`;
-
     humidity.innerHTML = `${weather_data.main.humidity}%`;
     wind_speed.innerHTML = `${weather_data.wind.speed}Km/H`;
 
     var condition = weather_data.weather[0].main;
     switch(condition){
         case 'Clouds':
-            weather_img.src = "/assets/cloud.png";
+            weather_img.src = "./assets/cloud.png";
             break;
         case 'Clear':
-            weather_img.src = "/assets/clear.png";
+            weather_img.src = "./assets/clear.png";
             break;
         case 'Rain':
-            weather_img.src = "/assets/rain.png";
+            weather_img.src = "./assets/rain.png";
             break;
         case 'Mist':
-            weather_img.src = "/assets/mist.png";
+            weather_img.src = "./assets/mist.png";
             break;
         case 'Snow':
-            weather_img.src = "/assets/snow.png";
+            weather_img.src = "./assets/snow.png";
             break;
 
     }
